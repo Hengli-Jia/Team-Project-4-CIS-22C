06991ce06573749683b7b2e63ebdd40044ad1f4c
KhoaChristian <minhkhoa3103@gmail.com>
2025-05-30 13:12:37 -0700

    Update BinaryNode.cpp

--- BinaryNode.cpp
+++ BinaryNode.cpp
@@ -1,5 +1,18 @@
-class BinaryNode {
-  
+class* BinaryNode {
+private:
+  int* left;
+  int* right;
+  int key;
+  int index;
 
+public: 
+  int* setLeft();
+  int* setRight();
+  int* getLeft();
+  int* getRight();
+  int setKey();
+  int getKey();
+  int setIndex();
+  int getIndex();
 
 }
