f1313ed22b1f62a4443649c7cbe4baf56db53d18
KhoaChristian <minhkhoa3103@gmail.com>
2025-05-30 13:05:58 -0700

    Create BST.h

---
+++ BST.h
@@ -0,0 +1,13 @@
+class BinaryTree {
+private:
+  BinaryNode* nodePtr; 
+  int key;
+  int index;
+
+public: 
+  void insertBST ();
+  void deleteBST ();
+  void inorderTraversal ();
+  void indetedTree();
+};
+
