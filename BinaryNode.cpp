b244c7b7e7b1eb5b9a49031290ee97e30aa8699e
KhoaChristian <minhkhoa3103@gmail.com>
2025-05-31 15:21:22 -0700

    Update BinaryNode.cpp

--- BinaryNode.cpp
+++ BinaryNode.cpp
@@ -1,10 +1,10 @@
-# ifndef "BINARYNODE.h"
-# define "BINARYNODE.h"
 
 # include <iostream>
+# include "Puzzle.h"
 
 class* BinaryNode {
 private:
+  Puzzle puzzle;
   int* left;
   int* right;
   int key;
@@ -14,7 +14,7 @@ public:
   BinaryNode() { left = right = nullptr; key = -1, index = -1};
   BinarNodde (left, right, key, index) { this -> left = leftl; this -> right = right; this -> key = key; this -> index = index;}
   
-int* setLeft(BinaryNode* Ptr) {this -> left = Ptr;}
+  int* setLeft(BinaryNode* Ptr) {this -> left = Ptr;}
   int* setRight(BinaryNode* Ptr) {this -> right = Ptr;}
   int* getLeft() {return left;}
   int* getRight() { return right;}
@@ -22,5 +22,7 @@ int* setLeft(BinaryNode* Ptr) {this -> left = Ptr;}
   int getKey() {return key;}
   int setIndex(const& Puzzle puzzle) {puzzle -> index = index;}
   int getIndex() { return index;}
+
+  bool isLeaf () { return this -> left == nullptr && this -> right = nullptr;}
 
 }
