47385ede01f98bc5a4144a59f14e2d92715b44ca
KhoaChristian <minhkhoa3103@gmail.com>
2025-05-30 23:19:59 -0700

    Update BST.h

--- BST.h
+++ BST.h
@@ -3,11 +3,15 @@ private:
   BinaryNode* nodePtr; 
   int key;
   int index;
+  void _insertBST (const& string, const& string) const;
+  void _deleteBST (const& string) const;
+  void _inorderTraversal (void visit(string&), BinaryNode*) const;
+  void _indetedTree(void visit (string&), BinaryNode*, int level) const;
 
 public: 
-  void insertBST ();
-  void deleteBST ();
-  void inorderTraversal ();
-  void indetedTree();
+  void insertBST (const& string newPuzzle) { _insertBST(newPuzzle) ;}
+  void deleteBST (const& string Puzzle) {_deleteBST(Puzzle);}
+  void inorderTraversal (void visit(Puzzle currPuzzle), BinaryNode*) {_inorderTraversal (visit(currPuzzle), BinaryNode*) const;}
+  void indetedTree(BinaryNode*) {_indetedTree(void visit (visit (string&), BinaryNode*, int level) const;} 
 };
 
