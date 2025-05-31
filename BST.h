31f204226ce1bef405e8d14795f0d3a8f1f289d6
KhoaChristian <minhkhoa3103@gmail.com>
2025-05-31 15:16:26 -0700

    Update BST.h

--- BST.h
+++ BST.h
@@ -6,12 +6,15 @@
 class BinaryTree {
 private:
   BinaryNode* nodePtr; 
+  int count;
   void _insertBST (const& Puzzle item) const;
   void _deleteBST (const& Puzzle item) const;
   void _inorderTraversal (void visit(Puzzle& item), BinaryNode*) const;
   void _indetedTree (void visit (Puzzle&i tem), BinaryNode*, int) const;
 
 public: 
+  bool isEmpty () { return count == 0;}
+  int  getCount () {return count;}
   void insertBST (const& string newPuzzle) { _insertBST(newPuzzle) ;}
   void deleteBST (const& string Puzzle) {_deleteBST(Puzzle);}
   void inorderTraversal (void visit(Puzzle currPuzzle), BinaryNode*) {_inorderTraversal (visit, BinaryNode*);}
