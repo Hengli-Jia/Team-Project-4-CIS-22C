7184e96cc8e6a458ed716eba146f4fee6ee75986
KhoaChristian <minhkhoa3103@gmail.com>
2025-05-31 14:21:30 -0700

    Update BinaryNode.cpp

--- BinaryNode.cpp
+++ BinaryNode.cpp
@@ -9,13 +9,16 @@ private:
   int index;
 
 public: 
-  int* setLeft();
-  int* setRight();
+  BinaryNode() { left = right = nullptr; key = -1, index = -1};
+  BinarNodde (left, right, key, index) { this -> left = leftl; this -> right = right; this -> key = key; this -> index = index;}
+  
+int* setLeft(BinaryNode* Ptr) {this -> left = Ptr;}
+  int* setRight(BinaryNode* Ptr) {this -> left = Ptr;}
   int* getLeft() {return left;}
   int* getRight() { return right;}
-  int setKey();
+  int setKey(const& Puzzle puzzle) {puzzle -> key = key;}
   int getKey() {return key;}
-  int setIndex();
+  int setIndex(const& Puzzle puzzle) {puzzle -> index = index;}
   int getIndex() { return index;}
 
 }
