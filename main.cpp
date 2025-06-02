6adedc77f220e15443f4c0bf99593ae34a226dad
papereat <86267954+papereat@users.noreply.github.com>
2025-06-02 15:02:11 -0700

    Updated Main
    Added prototype for input data function where data will be inputed and the bst and hastables will be created
    Created menu fuction where the loop of the program will exist

--- main.cpp
+++ main.cpp
@@ -2,17 +2,109 @@
  TODO: add members information and project description
 */
 
-#include "Database.h"
+//#include "Database.h"
 #include "Puzzle.h"
+#include "hashtable.h"
+//#include "BST.h"
+
+#include <iostream>
+
+
+bool inputData(HashTable &hashTable, BinaryTree &bst,string inputFile);
+
+void menu();
+
 
 const void displayTeamMembers();
 void help();
-void exit();
+bool exit();
 
-int main() { return 0; }
+int main() 
+{
+    
+    HashTable hashTable;
+    BinaryTree bst;
+
+    string inputFile = 'puzzles_database.txt';
+
+    inputData(hashTable, bst, inputFile);
+
+
+
+}
 
 const void displayTeamMembers() {}
 
-void help() {}
+void help() 
+{
 
-void exit() {}
+}
+
+/*
+Asks user if they are sure they want to exit
+pre:
+    User input
+post:
+    true then exit
+    false then continue
+out:
+*/
+bool exit() 
+{
+    std::cout << "Are you sure you want to exit(Y): ";
+    char input;
+
+    std::cin >> input;
+
+    input = tolower(input);
+
+    return (input == 'y');
+}
+
+/*
+Using input file, create hastable and binary search tree contianing the data 
+pre:
+    inputFile: string is the name of the input file to be used to get that data
+post:
+    bool: If the input of data was succssful
+out:
+    hashtable: hash table that stores the data
+    bst: binary search tree that containes the keys to be used in the hashtable
+*/
+bool inputData(HashTable &hashTable, BinaryTree &bst,string inputFile)
+{
+    //Input data using puzzles_database.txt and output resulting hashtables and bst in the aprameters
+}
+
+
+void menu(HashTable &HashTable, BinaryTree &bst)
+{
+    //will loop until the user is done using the data
+    /*
+    - Add a new data item. 
+    missing
+    - Add data from an input file (get the name of the input file from the user).
+    missing
+    - Delete data (one item).
+    missing
+    - Find and display one element using the primary key.
+    missing
+    - List data sorted by the primary key.
+    missing
+    - Hidden print option (do not show it in the menu: details are given in Team Project-
+    Part2).
+    missing
+    - Write data to a file.
+    missing
+    - Statistics (details are given in Team Project-Part2)
+    missing
+    - Hidden option (do not show it in the menu): when selected, display the names of the
+    team members.
+    missing
+    - Help – to show the menu. Show the menu once, before the loop, then show the menu
+    upon request: “Enter an option (H – for help): ”
+    missing
+    - Exit
+    untested
+    */
+}
