7fef7c7591a6efb08f01ae016bede762248182c7
KhoaChristian <minhkhoa3103@gmail.com>
2025-06-06 18:47:18 -0700

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -3,20 +3,41 @@
 
 using namespace std;
 
-void _insertBST (const& Puzzle item) const {
+BinaryNode* _insertBST (BinaryNode* nodePtr, BinaryNode* newPtr) const {
+  BinaryNode* pwalk = nodePtr;
+  while (!pwalk -> isLeaf()) {
+    // parent = pwalk;
+  if (pwalk -> getItem() < newPtr.getItem() )
+    pwalk = pwalk -> getLeft();
+  else 
+    pwalk = pwalk -> getRight();
+  }
+  if (newPtr -> getItem() < pwalk.getItem())
+    pwalk -> setLeft(newPtr);
+  else 
+    pwalk -> setRight(newPtr);
+  return newPtr;
 }
 
 void _deleteBST (const& Puzzle item) const  {
 
   
 }
-void _inorderTraversal (void visit(Puzzle& item), BinaryNode*) const {
-
-
+void _inorderTraversal (void visit(Puzzle& item), BinaryNode* nodePtr) const {
+  Puzzle puzzle = nodePtr -> getItem();
+  if (nodePtr) {
+  _inorderTraversal (visit, nodePtr -> getLeft());
+  visit(puzzle);
+  _inordeTraversal (visit, nodePtr -> getRight());
+  }
 }
-void _indetedTree(void visit (Puzzle&), BinaryNode*, int level) const {
-
-
+void _indetedTree(void visit (Puzzle& item), BinaryNode* nodePtr, int level) const {
+  Puzzle puzzle = nodePtr -> getItem();
+  if (nodePtr) {
+  visit(puzzle);
+  _indentedTree(visit, nodePtr -> getRight(), level + 1);
+  _indentedTree (visit, nodePtr -> getLeft(), level + 1);
+  }
 }
 
 
