ee7e2a3b0bafd9d5de7f82ea43c1af3a9b0ac53d
KhoaChristian <minhkhoa3103@gmail.com>
2025-06-06 18:57:22 -0700

    Update BST.h

--- BST.h
+++ BST.h
@@ -7,7 +7,7 @@ class BinaryTree {
 private: 
   BinaryNode* rootPtr;
   int count;
-  void _insertBST (const& Puzzle item) const;
+  BinaryNode* _insertBST (BinaryNode*, BinaryNode*) const;
   void _deleteBST (const& Puzzle item) const;
   void _inorderTraversal (void visit(Puzzle& item), BinaryNode*) const;
   void _indetedTree (void visit (Puzzle&i tem), BinaryNode*, int) const;
@@ -15,10 +15,20 @@ private:
 public: 
   bool isEmpty () { return count == 0;}
   int  getCount () {return count;}
-  void insertBST (const& string newPuzzle) { _insertBST(newPuzzle) ;}
-  void deleteBST (const& string Puzzle) {_deleteBST(Puzzle);}
-  void inorderTraversal (void visit(Puzzle currPuzzle), BinaryNode*) {_inorderTraversal (visit, BinaryNode*);}
-  void indetedTree(void visit (Puzzle currPuzzle), BinaryNode*, int level) {_indetedTree(visit , BinaryNode*, 1);} 
+  bool insertBST (const& string newPuzzle) { 
+    BinaryNode* newNode = new BinaryNode(newPuzzle);
+    this -> rootPtr = _insertBST(this -> rootPtr, newNode) ;
+    return true;
+  }
+  void deleteBST (const& string Puzzle) {
+    BinaryNode* newNode = new BinaryNode(newPuzzle);
+    if (_deleteBST(root-> Ptr, newNode))
+    return true;
+    else 
+    return false;
+  }
+  void inorderTraversal (void visit(Puzzle currPuzzle), BinaryNode*) {_inorderTraversal (visit, rootPtr);}
+  void indetedTree(void visit (Puzzle currPuzzle), BinaryNode*, int level) {_indetedTree(visit , rootPtr, 1);} 
 };
 
 #endif
